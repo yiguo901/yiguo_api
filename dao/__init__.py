5d3c4465da0ba0fec9df624d35a0ece6db664286
felix0303 <yyz315@qq.com>
2019-06-27 15:59:52 +0800

    日常更新

--- dao/__init__.py
+++ dao/__init__.py
@@ -1,28 +1,24 @@
 import pymysql
 from pymysql.cursors import DictCursor
-
 from logger import api_logger
 
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': 'root',
-    'password': 'yg1176',
-    'db': 'yg_api_db',
-    # 'password': '123456',
-    # 'db': 'yiguo',
-    'charset': 'utf8'
+    "host":"localhost",
+    "port":3306,
+    "user":"root",
+    "password":"123456",
+    "db":"yiguo",
+    "charset":"utf8"
 }
 
 
 class DB:
     def __init__(self):
         self.conn = pymysql.Connect(**DB_CONFIG)
-        # 如果上传的code中包含更新sql语句，如何自动创建(在服务器端)
 
     def __enter__(self):
         if self.conn is None:
-            # 考虑数据库连接是断开的情况
             self.conn = pymysql.Connect(**DB_CONFIG)
 
         return self.conn.cursor(cursor=DictCursor)
@@ -34,50 +30,56 @@ class DB:
             api_logger.error(exc_val)
             self.conn.rollback()
 
-        return True  # 异常不会继续向外抛出
+        return True # 异常不会继续向外抛出
 
 
 class BaseDao():
     def __init__(self):
         self.db = DB()
 
-    def save(self, table_name, **values):
-        # insert or update
-        # values['id'] 如果ID值是存在的，则是更新, 反之是插入
-        sql = 'insert into %s(%s) values(%s)' % \
+    def save(self,table_name,**values):
+        sql = 'replace into %s(%s) values(%s)' % \
               (table_name,
                ','.join(values.keys()),
-               ','.join([ '%%(%s)s' % key for key in values.keys()])
+               ','.join(['%%(%s)s' % key for key in values.keys()])
                )
-
+        success = False
         with self.db as c:
-            c.execute(sql, args=values)
+            c.execute(sql,args=values)
+            api_logger.info('replace %s ok!' % sql)
+            success = True
+        return success
 
-        # api_logger.info('insert %s ok!' % sql)
-
-
-    def delete(self, talbe_name, by_id):
-        sql = 'delete from %s where id=%s'%(talbe_name, by_id)
-        with self.db as c:
-            c.execute(sql)
-        
-    def list(self, table_name,
-             *fields, where=None, args=None,
-             page=1, page_size=20):
+    def delete(self,table_name,by_id):
         pass
 
-    def count(self, table_name):
-        pass
-    
-    def query(self, table, *fields, page=1):
-        if not len(fields):
-            sql = 'select * from %s limit %s' % (table, page)
-        else:
-            sql = 'select %s from %s limit %s' % (','.join(fields), table,page)
-            
-        print(sql)
+    def list(self,table_name,*fileds,
+             where=None,args=None,page=1,page_size=20):
+        sql = "select {} from {} where {}={} limit {},{}".format\
+            (','.join(*fileds),table_name,where,args,(page-1)*page_size,page_size)
         with self.db as c:
             c.execute(sql)
+            result = c.fetchall()
+            api_logger.info('select %s ok!' % sql)
+            return result
+
+    def count(self,table_name):
+        pass
+
+    def query(self,sql,*args):
+        with self.db as c:
+            c.execute(sql,args=args)
             data = c.fetchall()
+            if data:
+                data = list(data)
         return data
-    
+
+if __name__ == '__main__':
+    user_data = {'u_phone': 18691995899, 'u_auth_string': 'feilix6',
+                 #'nickname': '18691995899',
+                 #'gender':True,
+                 # 'img':'1.jpg',
+                 # 'balance':123.2,
+                 # 'level':'VIP'
+                 }
+    print(BaseDao().save('users', **user_data))
