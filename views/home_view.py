5c3bff88edda03b6d1d1e8b2a12bc34d0a02ef23
wdong123 <976426456@qq.com>
2019-06-28 16:04:10 +0800

    '完成测试界面加载'

--- views/home_view.py
+++ views/home_view.py
@@ -13,60 +13,53 @@ def home_view():
 	dao = home_dao()
 	img_wheel_datas = dao.query_all('wheel')
 	img_nav_datas = dao.query_all('nav')
+	img_chosen_datas = dao.query_all('chosen')
 
+	# 查询导航的数据
 	
-
-	type = img_nav_datas['trackid']
-	img_tpye_datas = dao.query_limit('goods',('name','goods_wheel_img','price'),page=8)
-	for type_img_data in img_nav_datas:
-		type_img = type['goods_wheel_img'].split('#')
-		type_img_data['goods_wheel_img'] = type_img[0]
-		
-	goodlist = []
-	for nav in img_nav_datas:
-		for i in range(8):
-			nav['imgnext'] = img_tpye_datas[i]
-			goodlist.append(nav)
-		
-	type_detail = dao.query_type(page=8)
-	for type in type_detail:
-		img = type['goods_wheel_img'].split('#')
-		type['goods_wheel_img'] = img[0]
-		
-		
-	img_chosen_datas = dao.query_limit(('id','img, trackid'), 'choosen')
-	list_img = []
-	list_img_datas = dao.query_type(page=6)
-	for list_img_data in list_img_datas:
-		img = list_img_data['goods_wheel_img'].split('#')
-		list_img_data['goods_wheel_img'] = img[0]
-	list_img.append(list_img_datas)
-	for img_chosen_data in img_tpye_datas:
-		for img in range(len(list_img)):
-			img_nav_datas['imgtext'] = list_img[img]
-			
-		
+	# 导航详情列表
+	for img_chosen_data in img_chosen_datas:
+		nav_trackid = img_chosen_data['trackid']
+		nav_groups = dao.query_group('goods',nav_trackid)
+		for nav_group in nav_groups:
+			img_group = nav_group['goods_wheel_img'].split('#')
+			nav_group['goods_wheel_img'] = img_group[0]
+		img_chosen_data['imglist'] = nav_groups
+	#导航的另一种结构
+	nav_type_details = dao.query_type_nav()
+	print(")))",nav_type_details)
+	for nav_type_detail in nav_type_details:
+		nav_type_category_id = nav_type_detail['category_id']
+		nav_type_lists = dao.query_group('goods',nav_type_category_id,8)
+		for nav_type_list in nav_type_lists:
+			img_group = nav_type_list['goods_wheel_img'].split('#')
+			nav_type_list['goods_wheel_img'] = img_group[0]
 	
-
+		nav_type_detail['imglist'] = nav_type_lists
 		
 		
+	print("&&&&&&&&&&",nav_type_details)
+	
+	
 	return jsonify({
 		'code': 8000,
 		'msg': 'ok',
 		'data_wheel': img_wheel_datas,
 		'data_nav': img_nav_datas,
 		'data_chosen': img_chosen_datas,
-		'data_type':type_detail,
-		'goodlist':goodlist,
-		
-		
-		
+		'data_nav_type':nav_type_details
+		# 'goodlist':goodlist,
 	})
 
 @blue_home.route('/home/index/<child_id>/',methods=("GET",))
-def nav_view(child_id):
+#导航类列表表详情
+def nav_list_view(child_id):
 	dao = home_dao()
-	nav_datas = dao.query_nav('goods',child_id)
+	nav_datas = dao.query_group('goods',child_id)
+
+	for detail_data in nav_datas:
+		img = detail_data['goods_wheel_img'].split('#')
+		detail_data['goods_wheel_img'] = img[0]
 	return jsonify({
 		'code': 8000,
 		'msg': 'ok',
@@ -75,6 +68,7 @@ def nav_view(child_id):
 	})
 	
 @blue_home.route('/home/eat/', methods=("GET",))
+#吃饭吧详情
 def eat_view():
 	dao = home_dao()
 	img_eat_datas = dao.query_all('ygeat')
@@ -86,11 +80,13 @@ def eat_view():
 	})
 
 @blue_home.route('/detail/<child_id>/',methods=("GET",))
+#商品详情页面
 def detail_view(child_id):
 	dao = home_dao()
-	detail_datas = dao.query_limit(('name','goods_wheel_img',
-	                                'price','pro_addr','sale',
-	                                'stock','goods_wheel_img'),'goods',page=1)
+	detail_datas = dao.query_detail(child_id)
+	for detail_data in detail_datas:
+		img = detail_data['goods_wheel_img'].split('#')
+		detail_data['goods_wheel_img'] = img[0]
 	return jsonify({
 		'code': 8000,
 		'msg': 'ok',
@@ -100,9 +96,11 @@ def detail_view(child_id):
 
 
 @blue_home.route('/detail/img/<string:id>/', methods=("GET",))
+#商品图片详情页面
 def detail_img_view(id):
 	dao = home_dao()
-	detail_datas = dao.query_limit(('detail_img_url'), 'goods', page=1)
+	detail_datas = dao.query_detail(id)
+	
 	return jsonify({
 		'code': 8000,
 		'msg': 'ok',
@@ -112,11 +110,33 @@ def detail_img_view(id):
 	
 	
 	
-
+@blue_home.route('/type/<num>/',methods=("GET",))
+def type_view(num='1001'):
+	dao = home_dao()
+	type_datas = dao.query_type()
 	
+	if num == '1001':
+		type_category_id = type_datas[0]['category_id']
+		type_detail_datas = dao.query_group_all('goods',type_category_id)
+		for type_detail_data in type_detail_datas:
+			img = type_detail_data['goods_wheel_img'].split('#')
+			type_detail_data['goods_wheel_img'] = img[0]
+	else:
+		type_detail_datas = dao.query_group_all('goods',num)
+		for type_detail_data in type_detail_datas:
+			img = type_detail_data['goods_wheel_img'].split('#')
+			type_detail_data['goods_wheel_img'] = img[0]
+		
+	print(type_datas)
+	
+	
+	return jsonify({
+		'code': 8000,
+		'msg': 'ok',
+		'data_wheel': type_datas,
+		'type_detail_datas':type_detail_datas
+		
+		
+	})
 
-if __name__ == '__main__':
-	dao = UserDao()
-	img = dao.query('wheel','id','img')
 
-	print(img)
