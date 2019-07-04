c2c77ea8b3c6ecd7085d25c8f10a72c4e264722f
shencangcang <577086979@qq.com>
2019-07-04 11:43:25 +0800

    test user/order

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,11 @@
 from views import home_view, cart_view, user_view, order_view
+from flask import request
+
+
+def check_requirments(method="GET", *params):
+    # 检查必要参数
+    data = request.args if method == "GET" else request.form
+    msg = {param: "必须提供"
+           for param in params if data.get(param, None) is None}
+
+    return msg
